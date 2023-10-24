6fe99679a185e3d7e14870f42925a00ee9dde20e
Jon <jmiguelmag@educacion.navarra.es>
2023-10-24 16:59:27 +0200

    Reorganizado el trabajo

--- SAAU/python/ejercicio01.py
+++
@@ -1,9 +0,0 @@
-num1 = float(input("Introduce el primer numero: "))
-num2 = float(input("Introduce el segundo numero: "))
-
-if num1 > num2:
-    print("El mayor numero es:", num1)
-elif num2 > num1:
-    print("El mayor numero es:", num2)
-else:
-    print("Los numero son iguales.")
